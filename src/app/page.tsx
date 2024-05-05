1282e7709e05f50d4924d9783be056c6c9030395
Ronaldo Ballecer <balleceronaldo@gmail.com>
2024-05-05 15:55:55 +0800

    l

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,8 +33,8 @@ export default function Home() {
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
           src="./bakcen.svg"
           alt="Next.js Logo"
-          width={180}
-          height={37}
+          width={400}
+          height={400}
           priority
         />
       </div>
