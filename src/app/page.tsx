2c120d7ad3471ce7036cc1396e1d868b79b13357
Ronaldo Ballecer <balleceronaldo@gmail.com>
2024-05-21 11:02:51 +0800

    text changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -52,7 +52,7 @@ export default function Home() {
             </span>
           </h2>
           <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
+            Find in-depth information about our E-Waste facility.
           </p>
         </a>
 
@@ -69,7 +69,7 @@ export default function Home() {
             </span>
           </h2>
           <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
+            Learn about hazardous waste found in electrical and electronic waste.
           </p>
         </a>
 
@@ -80,13 +80,13 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
+            Contribute{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
+            Explore how you can contribute to the cause of e-waste recycling.
           </p>
         </a>
 
@@ -97,13 +97,13 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
+            Educate{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+            Educate others regarding hazardous chemicals in waste electrical and electronic equipments.
           </p>
         </a>
       </div>
