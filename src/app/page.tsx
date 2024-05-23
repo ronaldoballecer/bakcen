db44b1ded19b29ab23bbdcad6329eb7f43fea1c0
Ronaldo Ballecer <balleceronaldo@gmail.com>
2024-05-23 18:55:10 +0800

    edits

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+// src/app/page.tsx
 import Image from "next/image";
 
 export default function Home() {
@@ -16,7 +17,7 @@ export default function Home() {
           >
             By {" "}
             <Image
-              src="./ballecerdigital.svg"
+              src="/ballecerdigital.svg"
               alt="Ballecer Digital Logo"
               className="dark:invert"
               width={100}
@@ -30,7 +31,7 @@ export default function Home() {
       <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="./bakcen.svg"
+          src="/bakcen.svg"
           alt="Next.js Logo"
           width={400}
           height={400}
@@ -104,6 +105,21 @@ export default function Home() {
           </h2>
           <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
             Educate others regarding hazardous chemicals in waste electrical and electronic equipments.
+          </p>
+        </a>
+        
+        <a
+          href="/about"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+        >
+          <h2 className="mb-3 text-2xl font-semibold">
+            About Us{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className="m-0 max-w-[30ch] text-sm opacity-50">
+            Learn more about our mission and vision.
           </p>
         </a>
       </div>
